94370c858811e500b5dcf176eb872a965f3f4c19
Jensen Eicher <jeicher@mymail.mines.edu>
2021-05-02 17:07:42 -0600

    added night mode

--- app/src/main/java/com/csci4480/regiftcard/MainActivity.kt
+++ app/src/main/java/com/csci4480/regiftcard/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.csci4480.regiftcard
 
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -16,18 +21,25 @@ import com.csci4480.regiftcard.ui.ProfileActivity
 import com.csci4480.regiftcard.utils.FirebaseMethods
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
     companion object {
         private const val LOG_TAG = "448.MainActivity"
     }
 
     private var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var sensorManager: SensorManager
+    private lateinit var light : Sensor
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(LOG_TAG, "onCreate() called")
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
@@ -58,5 +70,34 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        Log.d(LOG_TAG, "sensor changed")
+
+
+        if (event != null) {
+            if( event.sensor.getType() == Sensor.TYPE_LIGHT) {
+                Log.d(LOG_TAG, "got Light sensor data of ${event.values[0]}")
+                var light = event.values[0];
+                if (light < 25000)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                if (light > 25000)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, light,
+                SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }
--- app/src/main/java/com/csci4480/regiftcard/ui/home/HomeFragment.kt
+++ app/src/main/java/com/csci4480/regiftcard/ui/home/HomeFragment.kt
@@ -1,13 +1,20 @@
 package com.csci4480.regiftcard.ui.home
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.csci4480.regiftcard.R
+import com.csci4480.regiftcard.databinding.ActivityProfileBinding
+import com.csci4480.regiftcard.ui.dashboard.DashboardViewModel
 
 class HomeFragment : Fragment() {
     companion object {
@@ -15,6 +22,7 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var homeViewModel: HomeViewModel
+    private var pic: ImageView? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -25,10 +33,15 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.activity_profile, container, false)
+
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+        var binding = ActivityProfileBinding.inflate(layoutInflater)
+        var view = binding.root
         //val textView: TextView = root.findViewById(R.id.text_dashboard)
 //        homeViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
-        return root
+        return view
     }
 }
